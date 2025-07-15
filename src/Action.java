b7aa714da358105aee76ad77486297aec487e508
sullyham <himynameissullyham@gmail.com>
2025-07-15 18:37:28 -0400

    Messed around with cardlayout and actionlisteners for the diary list

--- src/Action.java
+++ src/Action.java
@@ -1,9 +1,21 @@
+import javax.swing.*;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class Action implements ActionListener {
-    @Override
-    public void actionPerformed(ActionEvent e) {
+public class Action implements ListSelectionListener {
+    JPanel panel;
+    public void pass(JPanel panel){
+        this.panel = panel;
+    }
 
+    @Override
+    public void valueChanged(ListSelectionEvent e) {
+        if(e.getValueIsAdjusting()){
+            System.out.println("SWITCHED!");
+            panel.
+        }
     }
 }
--- src/MyFrame.java
+++ src/MyFrame.java
@@ -5,6 +5,7 @@ import javax.swing.event.ListSelectionListener;
 import java.awt.*;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseListener;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.concurrent.Flow;
 
@@ -20,15 +21,24 @@ List data = new List();
 
 
 
-
         JPanel title = new JPanel();
         title.setLayout(new BorderLayout());
+
         JLabel top = new JLabel("My Diary");
         top.setFont(new Font("Serif", Font.PLAIN, 30));
         top.setHorizontalAlignment(SwingConstants.CENTER);
         title.add(top, BorderLayout.CENTER);
         title.setBackground(Color.PINK);
         title.setPreferredSize(new Dimension(100,100));
+
+        JPanel center = new JPanel();
+        center.setLayout(new CardLayout());
+        JPanel tv = new JPanel();
+        tv.add(new JLabel("HEHEH"));
+        JPanel tvv = new JPanel();
+        tvv.add(new JLabel("ERWERWE"));
+        center.add(tv);
+        center.add(tvv);
 
         JPanel sidemenu = new JPanel();
         sidemenu.setLayout(new BoxLayout(sidemenu, BoxLayout.Y_AXIS));
@@ -42,16 +52,20 @@ List data = new List();
         JScrollPane listscroll = new JScrollPane(list1);
         listscroll.setPreferredSize(new Dimension(100,200));
         sidemenu.add(listscroll);
+
         JButton add = new JButton("+");
         Add adder = new Add();
         add.addActionListener(adder);
         adder.pass(list);
         sidemenu.add(add);
-
         add(title, BorderLayout.NORTH);
         add(sidemenu, BorderLayout.WEST);
-            int selected = list1.getSelectedIndex();
+        add(center);
 
+        int selected;
+        Action action = new Action();
+        action.pass(center);
+        list1.addListSelectionListener(action);
 
 
     }
